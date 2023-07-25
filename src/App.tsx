a144f79aa0fb2e9539c0e296712503a1dd36e1bc
Sarin Yongsuwan <ballsarin@MacBookball.local>
2023-07-25 12:01:20 +0700

    add react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload??.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="mt-4">
+        <Col>
+          <h1>Hello, React-Bootstrap!</h1>
+          <p>This is a sample text using React-Bootstrap.</p>
+          <Button variant="primary">Click Me</Button>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
