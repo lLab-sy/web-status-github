148bcc96df5627dc03aa12bcb6fcfa59d26a7624
Sarin Yongsuwan <ballsarin@MacBookball.local>
2023-07-27 20:58:30 +0700

    change header ui

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,11 +3,10 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
-    <Navbar className="bg-body-tertiary fixed-top">
-      <Container>
+    <Navbar className="bg-body-tertiary fixed-top" style={{width : "100%"}}>
+      <Container style={{display: 'flex', justifyContent: "center", width: "100%"}}>
         <Navbar.Brand href="#home">Web Status Github</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-center">
+        <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
             Signed in as: <a href="#login">Username</a>
           </Navbar.Text>
--- src/containers/MainContent/index.tsx
+++ src/containers/MainContent/index.tsx
@@ -2,19 +2,19 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import FrequencyGraph from '../../components/frequency-graph';
-import AllProject from '../../components/numbers/all-project';
+import RightSiderContent from '../../components/right-sider-content';
 
 function MainContent() {
   return (
     <Container style={{ display: "block", height: "100vh", paddingTop: "100px" }}>
       <Row style={{ margin: "10px" }}>
         <Col sm={9}><FrequencyGraph /></Col>
-        <Col sm={3} md="auto" style={{ display: 'flex', justifyContent: 'space-between', height: '100%', flexDirection: 'column'}}>
-            <AllProject title='All Project' value='5 projects' />
-            <AllProject title='Most Language' value='Java' />
-            <AllProject title='Organizations' value='1' />
-            <AllProject title='following / followers' value='0/1' />
-          </Col>
+        <Col sm={3} md="auto" style={{ display: 'flex', justifyContent: 'space-between', height: '100%', flexDirection: 'column' }}>
+          <RightSiderContent title='All Project' value='5 projects' />
+          <RightSiderContent title='Most Language' value='Java' />
+          <RightSiderContent title='Organizations' value='1' />
+          <RightSiderContent title='following / followers' value='0/1' />
+        </Col>
       </Row>
     </Container>
   );
